ae37513022ba8a7d8287ad943f258537d914169c
Vineelkrishna <vineelkrishnakothapalli@gmail.com>
2026-08-09 21:39:53 +0530

    Time: 1 ms (94.74%) | Memory: 46.4 MB (51.98%) - LeetSync

--- 4354-unique-middle-element/unique-middle-element.java
+++ 4354-unique-middle-element/unique-middle-element.java
@@ -1,24 +1,14 @@
 class Solution {
     public boolean isMiddleElementUnique(int[] nums) {
-        int n = nums.length;
-         int middle = n/2;
-         int cnt=0;
-         int val = nums[middle];
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i],1);
-            }
-            else{
-                map.put(nums[i],map.get(nums[i])+1);
-            }
-           
-           
-        }
-        if(map.get(nums[middle])>1){
-            return false;
-        }
-        return true;
-        
+        int val = nums[nums.length / 2];
+int count = 0;
+
+for (int num : nums) {
+    if (num == val) {
+        count++;
+    }
+}
+
+return count == 1;
     }
 }
